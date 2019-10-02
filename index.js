864fcd35d3645f1f9040053553edd14f2eb3d0b0
Antonio Gabriel <saplemtr@gmail.com>
2019-10-02 19:20:48 -0300

    Added Main Form

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ import {name as appName} from './app.json';
 
 //app native forms
 import LoginForm from './src/components/forms/login_form'
+import MainForm from './src/components/forms/main_form'
 
 export default class Demo extends Component {
   render() {
     return (
       
-      <LoginForm />
+      //<LoginForm />
+      <MainForm/>
     );
   }
 }
---
+++ src/components/forms/main_form.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, ToolbarAndroid, Image, Dimensions } from 'react-native'
+import PropTypes from 'prop-types'
+/**
+* @author Antonio Souza
+* @class MainForm
+**/
+class MainForm extends Component {
+  state = {}
+
+  render() {
+    const { container } = styles
+    return (
+      <View style={container}>
+        <View style={styles.mainFormContend}>
+
+          {/* <View style={styles.topMenu}>
+         <View  style={styles.topMenuAppTitle}>
+           <Text style={styles.topMenuAppTitleText}>+ Saúde</Text>
+         </View>
+</View> */}
+
+
+
+        </View>
+
+        <View style={styles.actionButtons}>
+
+          <TouchableOpacity style={styles.actionButton}>
+
+          </TouchableOpacity>
+
+        </View>
+
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+  },
+  mainFormContend: {
+
+  },
+  topMenu: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 70,
+    width: '100%',
+    backgroundColor: '#6000BF',
+    elevation: 8
+  },
+  topMenuAppTitleText: {
+    color: '#fff',
+    fontFamily: 'Manjari Regular',
+    fontSize: 40
+  },
+  actionButtons: {
+
+  },
+  actionButton: {
+
+  }
+})
+export default MainForm
