e8154b6a25efeb4362ff9067f1280107d33fffb6
Antonio Gabriel <saplemtr@gmail.com>
2019-10-05 19:47:55 -0300

    added button cards

--- src/components/forms/main_form.js
+++ src/components/forms/main_form.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, Dimensions, StatusBar } from 'react-native'
-import { Container, Header, Title, Button } from 'native-base';
+import { View, Text, StyleSheet, Image, Dimensions, PixelRatio, StatusBar } from 'react-native'
+import { Content, Header, Title, Button, Card, CardItem } from 'native-base';
+import Touchable from 'react-native-platform-touchable';
 
 import PropTypes from 'prop-types'
 
 import { APP_DEFAULT_COLOR_1, APP_STATUS_BAR_COLOR_2, APP_TEXT_COLOR_WHITE } from '../settings/app_settings';
+
+const widthPercentageToDP = widthPercent => {
+  const screenWidth = Dimensions.get('window').width;
+  return PixelRatio.roundToNearestPixel(screenWidth * parseFloat(widthPercent) / 100);
+};
+
+const heightPercentageToDP = heightPercent => {
+  const screenHeight = Dimensions.get('window').height;
+  return PixelRatio.roundToNearestPixel(screenHeight * parseFloat(heightPercent) / 100);
+};
 
 /**
 * @author Antonio Souza
@@ -17,7 +28,7 @@ class MainForm extends Component {
     const { container } = styles
     return (
       <View style={container}>
-        <View style={styles.mainFormContend}>
+        <View style={styles.mainFormContent}>
           <Header style={styles.topMenu}>
             <StatusBar backgroundColor={APP_STATUS_BAR_COLOR_2} barStyle="dark-content" hasTabs />
             <Button style={styles.topMenuUserButtonIcon} transparent>
@@ -30,7 +41,69 @@ class MainForm extends Component {
               {/* <Image style={styles.topMenuIcon} source={require('../resources/images/menu.png')} /> */}
             </Button>
           </Header>
-          <View style={styles.container}>
+          <View style={styles.collumnCard}>
+
+            <View style={styles.rowCard}>
+              <Touchable foreground={Touchable.Ripple(styles.cardButton1.backgroundColor)} style={styles.touchable}>
+                <Card style={styles.cardButton1}>
+                  <CardItem style={styles.cardIconItemContent} backgroundColor={styles.cardButton1.backgroundColor}>
+                    <Image style={styles.cardIconItem} source={require('../resources/images/calendar.png')} resizeMode='center' />
+                  </CardItem>
+                  <CardItem style={styles.cardTextItemContent} backgroundColor={styles.cardButton1.backgroundColor}>
+                    <Text styles={styles.cardText}>Consulta</Text>
+                  </CardItem>
+                </Card>
+              </Touchable>
+
+              {/* BOTÃO AGENDAMENTO */}
+              <Touchable foreground={Touchable.Ripple(styles.cardButton2.backgroundColor)} style={styles.touchable}>
+                <Card style={styles.cardButton2}>
+                  <CardItem style={styles.cardTitleContent} backgroundColor={styles.cardButton2.backgroundColor}>
+                    <Text style={styles.cardTitle}>Agendar</Text>
+                  </CardItem>
+                  <CardItem style={styles.cardIconItemContent} backgroundColor={styles.cardButton2.backgroundColor}>
+                     <Image style={styles.cardIconItem} source={require('../resources/images/calendar.png')} resizeMode='center' />
+                  </CardItem>
+                  <CardItem style={styles.cardInfoContent} backgroundColor={styles.cardButton2.backgroundColor}>
+                    <Text style={styles.cardInfo}>Faça uma consulta  com  data e hora marcadas.</Text>
+                  </CardItem>
+                </Card>
+              </Touchable>
+            </View>
+
+            <View style={styles.rowCard}>
+
+              {/* BOTÃO CARTEIRA VACINAÇÃO */}
+              <Touchable foreground={Touchable.Ripple(styles.cardButton3.backgroundColor)} style={styles.touchable}>
+                <Card style={styles.cardButton3}>
+                  <CardItem style={styles.cardTitleContent} backgroundColor={styles.cardButton3.backgroundColor}>
+                    <Text style={styles.cardTitle}>Vacinação</Text>
+                  </CardItem>
+                  <CardItem style={styles.cardIconItemContent} backgroundColor={styles.cardButton3.backgroundColor}>
+                     <Image style={styles.cardIconItem} source={require('../resources/images/id-card.png')} resizeMode='center' />
+                  </CardItem>
+                  <CardItem style={styles.cardInfoContent} backgroundColor={styles.cardButton3.backgroundColor}>
+                    <Text style={styles.cardInfo}>Abra  sua  carteira  de vacinação digital</Text>
+                  </CardItem>
+                </Card>
+              </Touchable>
+
+              {/* BOTÃO BUSCAR GPS */}
+              <Touchable foreground={Touchable.Ripple(styles.cardButton4.backgroundColor)} style={styles.touchable}>
+                <Card style={styles.cardButton4}>
+                  <CardItem style={styles.cardTitleContent} backgroundColor={styles.cardButton4.backgroundColor}>
+                    <Text style={styles.cardTitle}>Unidades</Text>
+                  </CardItem>
+                  <CardItem style={styles.cardIconItemContent} backgroundColor={styles.cardButton4.backgroundColor}>
+                     <Image style={styles.cardIconItem} source={require('../resources/images/map.png')} resizeMode='center' />
+                  </CardItem>
+                  <CardItem style={styles.cardInfoContent} backgroundColor={styles.cardButton4.backgroundColor}>
+                    <Text style={styles.cardInfo}>Faça uma busca de GPS por unidades de saúde mais proxima a você.</Text>
+                  </CardItem>
+                </Card>
+              </Touchable>
+
+            </View>
           </View>
 
         </View>
@@ -43,18 +116,17 @@ class MainForm extends Component {
 
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-  },
-  mainFormContend: {
-
+  mainFormContent: {
+    backgroundColor: '#fff'
   },
   topMenu: {
     position: 'relative',
     backgroundColor: APP_DEFAULT_COLOR_1,
     justifyContent: 'space-between',
     paddingLeft: 15,
-    paddingRight: 15
+    paddingRight: 15,
+    elevation: 0,
+    marginBottom: 5
   },
   topMenuIcon: {
     position: 'relative',
@@ -77,6 +149,87 @@ const styles = StyleSheet.create({
     color: APP_TEXT_COLOR_WHITE,
     height: 40,
     alignSelf: 'center'
+  },
+  rowCard: {
+    position: 'relative',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10
+  },
+  collumnCard: {
+    position: 'relative',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+  },
+  touchable: {
+    borderRadius: 23
+  },
+  cardButton1: {
+    position: 'relative',
+    width: Dimensions.get('window').width / 2 * 0.911111111,
+    height: Dimensions.get('window').height / 2 * 0.811111111,
+    borderRadius: 23,
+    elevation: 0,
+    borderColor: '#fff',
+    padding: 10,
+    backgroundColor: '#E39868'
+  },
+  cardButton2: {
+    position: 'relative',
+    width: Dimensions.get('window').width / 2 * 0.911111111,
+    height: Dimensions.get('window').height / 2 * 0.811111111,
+    borderRadius: 23,
+    elevation: 0,
+    borderColor: '#fff',
+    padding: 10,
+    backgroundColor: '#635C73'
+  },
+  cardButton3: {
+    position: 'relative',
+    width: Dimensions.get('window').width / 2 * 0.911111111,
+    height: Dimensions.get('window').height / 2 * 0.811111111,
+    borderRadius: 23,
+    elevation: 0,
+    borderColor: '#fff',
+    padding: 10,
+    backgroundColor: '#BF4E98'
+  },
+  cardButton4: {
+    position: 'relative',
+    width: Dimensions.get('window').width / 2 * 0.911111111,
+    height: Dimensions.get('window').height / 2 * 0.811111111,
+    borderRadius: 23,
+    elevation: 0,
+    borderColor: '#fff',
+    padding: 10,
+    backgroundColor: '#7587BF'
+  },
+  cardIconItemContent: {
+    position: 'relative',
+    justifyContent: 'center'
+  },
+  cardIconItem: {
+    height: 90,
+    width: 90,
+  },
+  cardTitleContent: {
+    justifyContent: 'center'
+  },
+  cardInfoContent: {
+    justifyContent: 'center'
+  },
+  cardTitle: {
+    fontFamily: 'FredokaOne Regular',
+    fontSize: 30,
+    color: '#fff'
+  },
+  cardInfo: {
+    fontFamily: 'Manjari Regular',
+    fontSize:18,
+    color: '#fff',
+    textAlign: 'justify'
   }
 })
 export default MainForm
---
+++ src/components/utils/main_form_side_bar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createDrawerNavigator, createAppContainer } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+Icon.loadFont();
